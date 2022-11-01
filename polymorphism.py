dfc58caef1d32f22b66819a7a063cf6a6a463b21
Georges Mbatou Charles DIEME <georgesmbatoucharles@gmail.com>
2022-11-01 23:51:36 +0000

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -12,20 +12,37 @@
 #         print("registering in progress...")
 
 
-email = input("please provide an email")
-password = 2022
-phone_number = input("please provide a number")
-date_of_birth = "22nd january"
+# email = input("please provide an email")
+# password = 2022
+# phone_number = input("please provide a number")
+# date_of_birth = "22nd january"
 
-class APPLE:
-    def registration(self,email,password):
-        print("Registration in progress...")
+# class APPLE:
+#     def registration(self,email,password):
+#         print("Registration in progress...")
 
-        if phone_number == 254:
-            print("Registration completed")
+#         if phone_number == 254:
+#             print("Registration completed")
 
         # if phone_number :''
         # print("Registration completed")
+
+
+email = ''
+phone_number = ''
+date_of_birth = ""
+username = ''
+class IBM:
+    def registration(self, email, username, phone_number = None ,date_of_birth = None):
+        print(f"email : {email}")
+        print(f"username : {username}")
+if phone_number != None and phone_number !='':
+    print("Please provide a phone number")
+if date_of_birth != None and date_of_birth != "":
+    print("Please provide your DOB")
+
+ibm = IBM()
+ibm.registration('nyawira@mest.org','gdieme')
 
 
    
